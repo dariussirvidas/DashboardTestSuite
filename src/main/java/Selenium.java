a6875fa157631fd3cb287d453982976aea0132ee
dariussirvidas <dsirvidas@yahoo.com>
2020-02-13 17:39:15 +0200

    moved drivers back to /drivers, json parsing for input and expected results, addDomain, test for addDomain

--- src/main/java/Selenium.java
+++ src/main/java/Selenium.java
@@ -5,7 +5,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
 
 class Selenium {
     private static WebDriver browser;
@@ -15,7 +18,7 @@ class Selenium {
      * Chrome 79
      */
     static void setupChrome() {
-        System.setProperty("webdriver.chrome.driver", "target\\classes\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
         browser = new ChromeDriver();
     }
 
@@ -39,13 +42,67 @@ class Selenium {
         browser.manage().window().maximize();
     }
 
+    static void implicitlyWait() {
+        browser.manage().timeouts().implicitlyWait(WAIT_TIME_SEC, TimeUnit.SECONDS);
+    }
+    static void implicitlyWait(int seconds) {
+        browser.manage().timeouts().implicitlyWait(seconds, TimeUnit.SECONDS);
+    }
+
     static void goToWebAddress(String address) {
         browser.get(address);
     }
 
-    static WebElement waitForClickableByCss(String selector) {
-        WebDriverWait waiter = new WebDriverWait(browser, WAIT_TIME_SEC);
+    static WebElement waitForClickableByCss(String selector, int seconds) {
+        WebDriverWait waiter = new WebDriverWait(browser, seconds);
         return waiter.until(ExpectedConditions.elementToBeClickable(By.cssSelector(selector)));
+    }
+
+    static WebElement findElementByCss(String selector) {
+        return browser.findElement(By.cssSelector(selector));
+    }
+
+    static WebElement findElementByName(String name) {
+        return browser.findElement(By.name(name));
+    }
+
+    static void addDomain(String serviceName, String url, String serviceType, String method, boolean auth,
+                          String user, String password, String parameters, String email, String checkInterval,
+                          boolean active) {
+        WebElement inputServiceName = findElementByCss("input[name=\"serviceName\"]");
+        WebElement inputUrl = findElementByCss("input[name=\"url\"]");
+        Select selectServiceType = new Select(findElementByCss("select[name=\"serviceType\"]"));
+        Select selectMethod = new Select(findElementByCss("select[name=\"method\"]"));
+        WebElement inputAuth = findElementByCss("input[name=\"auth\"]");
+        WebElement inputUser = findElementByCss("input[name=\"user\"]");
+        WebElement inputPassword = findElementByCss("input[name=\"password\"]");
+        WebElement textareaParameters = findElementByCss("textarea[name=\"parameters\"]");
+        WebElement inputEmail = findElementByCss("input[name=\"email\"]");
+        WebElement inputCheckInterval = findElementByCss("input[name=\"interval\"]");
+        WebElement inputActive = findElementByCss("input[name=\"active\"]");
+        WebElement buttonSubmit = findElementByCss("*[type=\"submit\"]");
+        System.out.println("submit button: " + buttonSubmit.getText());
+        inputServiceName.sendKeys(serviceName);
+        inputUrl.sendKeys(url);
+        selectServiceType.selectByVisibleText(serviceType);
+        selectMethod.selectByVisibleText(method);
+        if (auth) {          // assuming unchecked by default
+            inputAuth.click();
+        }
+        inputUser.sendKeys(user);
+        inputPassword.sendKeys(password);
+        textareaParameters.sendKeys(parameters);
+        inputEmail.sendKeys(email);
+        inputCheckInterval.sendKeys(checkInterval);
+        if (!active) {      // assuming checked by default
+            inputActive.click();
+        }
+        buttonSubmit.click();
+
+        try {
+            Thread.sleep(5000);      // <===== find a better wait method
+        }
+        catch (Exception ignored) {}
     }
 
     static void closeBrowser() {
--- src/test/java/SeleniumTests.java
+++ src/test/java/SeleniumTests.java
@@ -1,25 +1,78 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.WebElement;
 
+import java.io.FileReader;
+
 public class SeleniumTests {
+    private final String ADD_DOMAIN_FORM_DATA = "testData\\AddDomainFormData.json";
+    private final String INVALID_INPUT_BORDER_COLOR = "rgba(255, 0, 0, 1)";
+
     @BeforeClass
     public static void setupChrome() {
         Selenium.setupChrome();
         Selenium.maximizeBrowserWindow();
+        Selenium.implicitlyWait();
     }
 
     @Test
-    public void testSearch() {
-        Selenium.goToWebAddress("http://kitm.lt/");
-        WebElement search = Selenium.waitForClickableByCss("input[name=\"s\"]");
-        search.sendKeys("testuotojo");
-        search.submit();
+    public void testAddDomain() throws Exception {
+        Object data = readJson(ADD_DOMAIN_FORM_DATA).get(1);
+        Selenium.goToWebAddress("http://localhost:3000/domains");
+        Selenium.findElementByCss("#root > div:nth-child(2) > div > div > div > button").click();    //<========== button needs a unique class or name asap
+        addDomainFromJson(data);
+        if (isDataValid(data)) {
+            Assert.assertFalse("Modal is: open, expected: closed", isModalOpen());
+        }
+        else {
+            Assert.assertTrue("Modal is: closed, expected: open", isModalOpen());
+            JSONArray expectedFailedFields = getExpectedFailedFields(data);
+            for (Object expectedFailedField : expectedFailedFields) {
+                String fieldName = expectedFailedField.toString();
+                WebElement field = Selenium.findElementByName(fieldName);
+                String borderColor = field.getCssValue("border-top-color");
+                Assert.assertEquals(fieldName + " border color", INVALID_INPUT_BORDER_COLOR, borderColor); // <==== norim raudono border an inputo
+            }
+        }
     }
 
     @AfterClass
     public static void closeBrowser() {
         Selenium.closeBrowser();
+    }
+
+    private static JSONArray readJson(String filename) throws Exception {
+        FileReader reader = new FileReader(filename);
+        JSONParser jsonParser = new JSONParser();
+        return (JSONArray) jsonParser.parse(reader);
+    }
+
+    private static void addDomainFromJson(Object data) {
+        JSONObject dataJson = (JSONObject) data;
+        Selenium.addDomain(dataJson.get("serviceName").toString(), dataJson.get("url").toString(),
+                dataJson.get("serviceType").toString(), dataJson.get("method").toString(),
+                (Boolean) dataJson.get("auth"), dataJson.get("user").toString(), dataJson.get("password").toString(),
+                dataJson.get("parameters").toString(), dataJson.get("email").toString(),
+                dataJson.get("interval").toString(), (Boolean) dataJson.get("active"));
+    }
+
+    private static JSONArray getExpectedFailedFields(Object data) {
+        JSONObject dataJson = (JSONObject) data;
+        return (JSONArray) dataJson.get("expectedFailedFields");
+    }
+
+    private static boolean isDataValid(Object data) {
+        return getExpectedFailedFields(data).size() < 1;
+    }
+
+    private static boolean isModalOpen() {
+        String bodyClass = Selenium.findElementByCss("body").getAttribute("class");
+        System.out.println(bodyClass);
+        return bodyClass.equals("modal-open");
     }
 }
